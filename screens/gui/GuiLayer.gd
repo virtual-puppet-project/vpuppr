94c3e7fc805c9fdabea1d86e6a8e38ebb1ac4c42
Tim Yuen <timothy.yuen22@gmail.com>
2021-02-18 22:55:22 -0500

    add facetracker start button, refactor gui
    Former-commit-id: 1b004a348f429d63634448378056dbb224bba2f5

--- screens/gui/GuiLayer.gd
+++ screens/gui/GuiLayer.gd
@@ -4,10 +4,6 @@ extends CanvasLayer
 # Builtin functions                                                           #
 ###############################################################################
 
-func _ready() -> void:
-	$ControlBar/HBoxContainer/LoadVRMModelButton.connect("pressed", self, "_on_load_vrm_model_button_pressed")
-	$ControlBar/HBoxContainer/LoadBasicModelButton.connect("pressed", self, "_on_load_basic_model_button_pressed")
-
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_gui"):
 		for c in get_children():
@@ -16,22 +12,6 @@ func _input(event: InputEvent) -> void:
 ###############################################################################
 # Connections                                                                 #
 ###############################################################################
-
-func _on_load_vrm_model_button_pressed() -> void:
-	var model_selection_popup: FileDialog = load("res://screens/gui/ModelSelectionPopup.tscn").instance()
-	model_selection_popup.model_type = AppManager.ModelType.VRM
-	add_child(model_selection_popup)
-	
-	yield(model_selection_popup, "file_selected")
-	model_selection_popup.queue_free()
-
-func _on_load_basic_model_button_pressed() -> void:
-	var model_selection_popup: FileDialog = load("res://screens/gui/ModelSelectionPopup.tscn").instance()
-	model_selection_popup.model_type = AppManager.ModelType.GENERIC
-	add_child(model_selection_popup)
-	
-	yield(model_selection_popup, "file_selected")
-	model_selection_popup.queue_free()
 
 ###############################################################################
 # Private functions                                                           #
