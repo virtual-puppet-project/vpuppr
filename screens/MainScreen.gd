523a7204b684fba8049cef00401913c652c5af5c
Tim Yuen <timothy.yuen22@gmail.com>
2021-04-14 22:34:42 -0400

    Make OpenSee a singleton, tweak default environment values and expose some values in the UI, fix null pointer on changing models
    Former-commit-id: e865c0ef5172b7f548468a7e2eb0fdc9cf9afb51

--- screens/MainScreen.gd
+++ screens/MainScreen.gd
@@ -8,6 +8,7 @@ const MODEL_SCREEN: Resource = preload("res://screens/ModelDisplayScreen.tscn")
 var current_model_path: String = ""
 
 onready var light_container: Spatial = $LightContainer
+onready var world_environment: WorldEnvironment = $WorldEnvironment
 var model_display_screen: Spatial
 
 ###############################################################################
@@ -40,6 +41,9 @@ func _on_file_to_load_changed(file_path: String) -> void:
 
 func _clean_load_model_display_screen() -> void:
 	if model_display_screen:
+		# Prevent null pointers by pausing execution AND THEN freeing resources
+		model_display_screen.pause_mode = PAUSE_MODE_STOP
+		yield(get_tree(), "idle_frame")
 		model_display_screen.free()
 	model_display_screen = MODEL_SCREEN.instance()
 	model_display_screen.model_resource_path = current_model_path
