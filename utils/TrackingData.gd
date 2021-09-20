f56bf14b2f30617224bf1c3bad73a1db7276ed5d
Tim Yuen <timothy.yuen22@gmail.com>
2021-09-20 12:30:58 -0400

    Add interfaces for TrackingBackend and TrackingData
    Former-commit-id: 83bc20afef7602906f882ce4c62b50f8a812e815

---
+++ utils/TrackingData.gd
@@ -0,0 +1,68 @@
+class_name TrackingData
+extends Reference
+
+###############################################################################
+# Builtin functions                                                           #
+###############################################################################
+
+###############################################################################
+# Connections                                                                 #
+###############################################################################
+
+###############################################################################
+# Private functions                                                           #
+###############################################################################
+
+###############################################################################
+# Public functions                                                            #
+###############################################################################
+
+# Tracking metadata
+
+func get_updated_time() -> float:
+	AppManager.log_message("Not yet implemented", true)
+	return -1.0
+
+func get_fit_error() -> float:
+	AppManager.log_message("Not yet implemented", true)
+	return -1.0
+
+# General tracking data
+
+func get_rotation() -> Vector3:
+	AppManager.log_message("Not yet implemented", true)
+	return Vector3.ZERO
+
+func get_translation() -> Vector3:
+	AppManager.log_message("Not yet implemented", true)
+	return Vector3.ZERO
+
+# Eye data
+
+func get_left_eye_open_amount() -> float:
+	AppManager.log_message("Not yet implemented", true)
+	return -1.0
+
+func get_left_eye_gaze() -> Vector3:
+	AppManager.log_message("Not yet implemented", true)
+	return Vector3.ZERO
+
+func get_right_eye_open_amount() -> float:
+	AppManager.log_message("Not yet implemented", true)
+	return -1.0
+
+func get_right_eye_gaze() -> Vector3:
+	AppManager.log_message("Not yet implemented", true)
+	return Vector3.ZERO
+
+# Mouth data
+
+func get_mouth_open_amount() -> float:
+	AppManager.log_message("Not yet implemented", true)
+	return -1.0
+
+# Additional backend-specific data
+
+func get_additional_info() -> Reference:
+	AppManager.log_message("Not yet implemented", true)
+	return null
