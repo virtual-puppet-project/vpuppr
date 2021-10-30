ce26c01cff5aa1bb422209421e0bb7068b323966
Zutatensuppe <oo.para@gmail.com>
2021-10-30 12:58:38 +0200

    prepare some tests for usage with GUT
    Former-commit-id: 4772c430a81c315ff903ffbc0a27d0a7e0b23f31

--- test/unit/test_InterpolationData.gd
+++ test/unit/test_InterpolationData.gd
@@ -1,6 +1,12 @@
-extends BaseTest
+extends "res://test/base_test.gd"
 
 const INTERPOLATION_DATA: Resource = preload("res://utils/InterpolationData.gd")
+
+
+func before_all():
+	AppManager.cm = ConfigManager.new()
+	AppManager.cm.current_model_config = ConfigManager.ConfigData.new()
+	gut.p("ran run setup", 2)
 
 ###############################################################################
 # Builtin functions                                                           #
@@ -22,21 +28,30 @@ func test_update_values() -> void:
 	var input_target_rotation: Vector3 = Vector3(1, 1, 1)
 	var input_target_left_eye_rotation: Vector3 = Vector3(1, 1, 1)
 	var input_target_right_eye_rotation: Vector3 = Vector3(1, 1, 1)
+	var input_target_left_eye_blink: float = 1.0
+	var input_target_right_eye_blink: float = 1.0 
+	var input_target_mouth_movement: float = 1.0
 
 	interpolation_data.update_values(
 		input_updated,
 		input_target_translation,
 		input_target_rotation,
 		input_target_left_eye_rotation,
-		input_target_right_eye_rotation
+		input_target_right_eye_rotation,
+		input_target_left_eye_blink,
+		input_target_right_eye_blink,
+		input_target_mouth_movement
 	)
 
 	# Check stored data
-	assert(interpolation_data.last_updated == input_updated)
-	assert(interpolation_data.target_translation == input_target_translation)
-	assert(interpolation_data.target_rotation == input_target_translation)
-	assert(interpolation_data.target_left_eye_rotation == input_target_left_eye_rotation)
-	assert(interpolation_data.target_right_eye_rotation == input_target_right_eye_rotation)
+	assert_true(interpolation_data.last_updated == input_updated)
+	assert_true(interpolation_data.target_translation == input_target_translation)
+	assert_true(interpolation_data.target_rotation == input_target_translation)
+	assert_true(interpolation_data.target_left_eye_rotation == input_target_left_eye_rotation)
+	assert_true(interpolation_data.target_right_eye_rotation == input_target_right_eye_rotation)
+	assert_true(interpolation_data.target_left_eye_blink == input_target_left_eye_blink)
+	assert_true(interpolation_data.target_right_eye_blink == input_target_right_eye_blink)
+	assert_true(interpolation_data.target_mouth_movement == input_target_mouth_movement)
 
 func test_interpolate() -> void:
 	var interpolation_data = INTERPOLATION_DATA.new()
@@ -48,13 +63,19 @@ func test_interpolate() -> void:
 	var input_target_rotation: Vector3 = Vector3(1, 1, 1)
 	var input_target_left_eye_rotation: Vector3 = Vector3(1, 1, 1)
 	var input_target_right_eye_rotation: Vector3 = Vector3(1, 1, 1)
+	var input_target_left_eye_blink: float = 1.0
+	var input_target_right_eye_blink: float = 1.0 
+	var input_target_mouth_movement: float = 1.0
 
 	interpolation_data.update_values(
 		input_updated,
 		input_target_translation,
 		input_target_rotation,
 		input_target_left_eye_rotation,
-		input_target_right_eye_rotation
+		input_target_right_eye_rotation,
+		input_target_left_eye_blink,
+		input_target_right_eye_blink,
+		input_target_mouth_movement
 	)
 
 	var i_translation: Vector3 = interpolation_data.interpolate(interpolation_data.InterpolationDataType.TRANSLATION, interpolation_rate)
@@ -62,7 +83,7 @@ func test_interpolate() -> void:
 #	var i_left_eye_rotation: Vector3 = interpolation_data.interpolate(interpolation_data.InterpolationDataType.LEFT_EYE_ROTATION, interpolation_rate)
 #	var i_right_eye_rotation: Vector3 = interpolation_data.interpolate(interpolation_data.InterpolationDataType.RIGHT_EYE_ROTATION, interpolation_rate)
 
-	assert(i_translation == input_target_translation * interpolation_rate * interpolation_data.rate)
-	assert(i_rotation == input_target_rotation * interpolation_rate * interpolation_data.rate)
+	assert_true(i_translation == input_target_translation * interpolation_rate * interpolation_data.rate)
+	assert_true(i_rotation == input_target_rotation * interpolation_rate * interpolation_data.rate)
 #	assert(i_left_eye_rotation == input_target_left_eye_rotation * interpolation_rate * interpolation_data.rate)
 #	assert(i_right_eye_rotation == input_target_right_eye_rotation * interpolation_rate * interpolation_data.rate)
