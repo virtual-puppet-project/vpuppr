4772c430a81c315ff903ffbc0a27d0a7e0b23f31
Zutatensuppe <oo.para@gmail.com>
2021-10-30 12:58:38 +0200

    prepare some tests for usage with GUT

--- test/unit/test_JSONUtil.gd
+++ test/unit/test_JSONUtil.gd
@@ -1,4 +1,4 @@
-extends BaseTest
+extends "res://test/base_test.gd"
 
 const JSON_UTIL: Resource = preload("res://utils/JSONUtil.gd")
 
@@ -32,26 +32,30 @@ func test_transform_to_dictionary() -> void:
 	var output: Dictionary = json_util.transform_to_dictionary(input)
 	
 	# Check for required fields
-	assert(output["basis"], "No 'basis' field found.")
-	assert(output["origin"], "No 'origin' field found.")
+	assert_eq_deep(output["basis"], {
+		"x": { "x": 1.0, "y": 0.0, "z": 0.0 },
+		"y": { "x": 0.0, "y": 1.0, "z": 0.0 },
+		"z": { "x": 0.0, "y": 0.0, "z": 1.0 }
+	})
+	assert_eq_deep(output["origin"], { "x": 1.0, "y": 2.0, "z": 3.0 })
 
 	# Check basis
-	assert(output["basis"]["x"]["x"] == original_basis.x.x)
-	assert(output["basis"]["x"]["y"] == original_basis.x.y)
-	assert(output["basis"]["x"]["z"] == original_basis.x.z)
+	assert_true(output["basis"]["x"]["x"] == original_basis.x.x)
+	assert_true(output["basis"]["x"]["y"] == original_basis.x.y)
+	assert_true(output["basis"]["x"]["z"] == original_basis.x.z)
 
-	assert(output["basis"]["y"]["x"] == original_basis.y.x)
-	assert(output["basis"]["y"]["y"] == original_basis.y.y)
-	assert(output["basis"]["y"]["z"] == original_basis.y.z)
+	assert_true(output["basis"]["y"]["x"] == original_basis.y.x)
+	assert_true(output["basis"]["y"]["y"] == original_basis.y.y)
+	assert_true(output["basis"]["y"]["z"] == original_basis.y.z)
 
-	assert(output["basis"]["z"]["x"] == original_basis.z.x)
-	assert(output["basis"]["z"]["y"] == original_basis.z.y)
-	assert(output["basis"]["z"]["z"] == original_basis.z.z)
+	assert_true(output["basis"]["z"]["x"] == original_basis.z.x)
+	assert_true(output["basis"]["z"]["y"] == original_basis.z.y)
+	assert_true(output["basis"]["z"]["z"] == original_basis.z.z)
 
 	# Check origin
-	assert(output["origin"]["x"] == original_origin.x)
-	assert(output["origin"]["y"] == original_origin.y)
-	assert(output["origin"]["z"] == original_origin.z)
+	assert_true(output["origin"]["x"] == original_origin.x)
+	assert_true(output["origin"]["y"] == original_origin.y)
+	assert_true(output["origin"]["z"] == original_origin.z)
 
 func test_dictionary_to_transform() -> void:
 	var json_util = JSON_UTIL.new()
@@ -83,22 +87,22 @@ func test_dictionary_to_transform() -> void:
 	var output: Transform = json_util.dictionary_to_transform(input)
 
 	# Check basis
-	assert(output.basis.x.x == input["basis"]["x"]["x"])
-	assert(output.basis.x.y == input["basis"]["x"]["y"])
-	assert(output.basis.x.z == input["basis"]["x"]["z"])
+	assert_true(output.basis.x.x == input["basis"]["x"]["x"])
+	assert_true(output.basis.x.y == input["basis"]["x"]["y"])
+	assert_true(output.basis.x.z == input["basis"]["x"]["z"])
 
-	assert(output.basis.y.x == input["basis"]["y"]["x"])
-	assert(output.basis.y.y == input["basis"]["y"]["y"])
-	assert(output.basis.y.z == input["basis"]["y"]["z"])
+	assert_true(output.basis.y.x == input["basis"]["y"]["x"])
+	assert_true(output.basis.y.y == input["basis"]["y"]["y"])
+	assert_true(output.basis.y.z == input["basis"]["y"]["z"])
 	
-	assert(output.basis.z.x == input["basis"]["z"]["x"])
-	assert(output.basis.z.y == input["basis"]["z"]["y"])
-	assert(output.basis.z.z == input["basis"]["z"]["z"])
+	assert_true(output.basis.z.x == input["basis"]["z"]["x"])
+	assert_true(output.basis.z.y == input["basis"]["z"]["y"])
+	assert_true(output.basis.z.z == input["basis"]["z"]["z"])
 
 	# Check origin
-	assert(output.origin.x == input["origin"]["x"])
-	assert(output.origin.y == input["origin"]["y"])
-	assert(output.origin.z == input["origin"]["z"])
+	assert_true(output.origin.x == input["origin"]["x"])
+	assert_true(output.origin.y == input["origin"]["y"])
+	assert_true(output.origin.z == input["origin"]["z"])
 
 func test_color_to_dictionary() -> void:
 	var json_util = JSON_UTIL.new()
@@ -108,10 +112,10 @@ func test_color_to_dictionary() -> void:
 	var output: Dictionary = json_util.color_to_dictionary(input)
 
 	# Check color
-	assert(output["r"] == input.r)
-	assert(output["g"] == input.g)
-	assert(output["b"] == input.b)
-	assert(output["a"] == input.a)
+	assert_true(output["r"] == input.r)
+	assert_true(output["g"] == input.g)
+	assert_true(output["b"] == input.b)
+	assert_true(output["a"] == input.a)
 
 func test_dictionary_to_color() -> void:
 	var json_util = JSON_UTIL.new()
@@ -128,7 +132,7 @@ func test_dictionary_to_color() -> void:
 	var output: Color = json_util.dictionary_to_color(input)
 
 	# Check color
-	assert(output.r == salmon_color.r)
-	assert(output.g == salmon_color.g)
-	assert(output.b == salmon_color.b)
-	assert(output.a == salmon_color.a)
+	assert_true(output.r == salmon_color.r)
+	assert_true(output.g == salmon_color.g)
+	assert_true(output.b == salmon_color.b)
+	assert_true(output.a == salmon_color.a)
