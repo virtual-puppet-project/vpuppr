b52e4eb3d7483e65172113fc21fb1798ed25ffe2
Tim Yuen <timothy.yuen22@gmail.com>
2022-02-15 22:19:10 -0500

    refactor InterpolationData
    Former-commit-id: 8833989fbf105c86e287c665bdbea6a0ca1d1dff

--- screens/ModelDisplayScreen.gd
+++ screens/ModelDisplayScreen.gd
@@ -139,7 +139,8 @@ func _physics_process(_delta: float) -> void:
 					float(should_track_eye),
 			open_see_data.left_eye_open,
 			open_see_data.right_eye_open,
-			open_see_data.features.mouth_open
+			open_see_data.features.mouth_open,
+			open_see_data.features.mouth_wide
 		)
 
 	if apply_translation:
