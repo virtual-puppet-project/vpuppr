3df6892b7efc439e99b3a65f9c3798e0dbcab572
Tim Yuen <timothy.yuen22@gmail.com>
2022-06-16 17:34:33 -0400

    fix tests
    Former-commit-id: 6495c1060b92ba8ccbd69f516f7e98e64eb0dd15

--- tests/test_resources/extension_resources/good_extensions/test_extension/tracking_backend_dummy.gd
+++ tests/test_resources/extension_resources/good_extensions/test_extension/tracking_backend_dummy.gd
@@ -1,7 +1,5 @@
 extends TrackingBackendInterface
 
-var data = AM.em.load_resource("TestExtension", "tracking_data_dummy.gd").expect("Unable to load").new()
-
 func is_listening() -> bool:
 	return true
 
@@ -10,9 +8,6 @@ func start_receiver() -> void:
 
 func stop_receiver() -> void:
 	pass
-
-func get_data(_param = null) -> TrackingDataInterface:
-	return data
 
 func test_func() -> int:
 	return 10
