a1e6fe056244cbea4250ad1889a9baf50e5f4c50
Tim Yuen <timothy.yuen22@gmail.com>
2022-08-05 10:41:43 -0400

    Add translation manager, modify loading logic in extension manager, add translation manager errors, add stub for english translations

--- model/globals.gd
+++ model/globals.gd
@@ -74,6 +74,7 @@ const LANDING_SCREEN_PATH := "res://screens/landing_screen.tscn"
 const DEFAULT_RUNNER_PATH := "res://screens/default_runner.gd"
 const DEFAULT_GUI_PATH := "res://screens/gui/default_gui.tscn"
 const DEFAULT_MODEL_PATH := "res://entities/duck/duck.tscn"
+const TRANSLATIONS_PATH := "/resources/translations"
 
 const PROJECT_GITHUB_REPO := "https://github.com/virtual-puppet-project/vpuppr"
 const DISCORD_INVITE := "https://discord.com/invite/6mcdWWBkrr"
